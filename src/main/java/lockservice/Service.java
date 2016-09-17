2a1370726dcbfc08fbe8020b3d2eef1508041916
bluefox <asindor@gmail.com>
2016-09-17 18:59:18 +0300

    Косметичекие поправки

--- src/main/java/lockservice/Service.java
+++ src/main/java/lockservice/Service.java
@@ -2,14 +2,4 @@ package lockservice;
 
 public interface Service {
     void run(Object o);
-
-    public static void main(String[] args) {
-        Service s = null;
-        s.run(1);
-        s.run(2);
-        s.run("fsdfs");
-
-        s.run(2);
-        s.run(2);
-    }
 }
--- src/main/java/lockservice/StupidTest.java
+++ src/main/java/lockservice/StupidTest.java
@@ -11,21 +11,21 @@ import java.util.List;
 public class StupidTest {
     public static void main(String[] args) {
         List<Object> objects = new ArrayList<>();
-        for (int i = 0;i<10;i++) {
+        for (int i = 0; i < 10; i++) {
             objects.add(new Object());
         }
         Service service = o -> {
             int sum = 0;
-            for (int i=0;i<100_000_000;i++) {
-                sum+=i;
+            for (int i = 0; i < 100_000_000; i++) {
+                sum += i;
             }
-            System.err.println(Thread.currentThread().getName()+" "+sum);
+            System.err.println(Thread.currentThread().getName() + " " + sum);
         };
         EqualityLockService equalityLockService = new EqualityLockService(service);
-        for (int i = 0;i<50;i++) {
+        for (int i = 0; i < 50; i++) {
             final int ii = i;
-            Object randObj = objects.get((int)(Math.random()*objects.size()));
-            Thread thread = new Thread(()-> equalityLockService.run(randObj),"Thread num "+ii+" "+randObj.toString());
+            Object randObj = objects.get((int) (Math.random() * objects.size()));
+            Thread thread = new Thread(() -> equalityLockService.run(randObj), "Thread num " + ii + " " + randObj.toString());
             thread.start();
         }
     }
